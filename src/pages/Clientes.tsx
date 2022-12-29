ab8f0bbe3a3865e77ff1f68e266467e2f718664a
EduardoAyora <eduardoaayora64@gmail.com>
2022-12-29 00:32:14 -0500

    Editar cliente

--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -1,10 +1,12 @@
 import { IonButton, IonIcon, IonicSafeString, IonSpinner, useIonToast } from "@ionic/react";
 import { useEffect, useState } from "react"
 import { createOutline, trashOutline } from 'ionicons/icons';
+import RegistrarCliente from "./RegistrarCliente";
 
 export default function Clientes() {
   const [present] = useIonToast();
   const [clients, setClients] = useState<Cliente[]>()
+  const [clientEdited, setClientEdited] = useState<Cliente>()
 
   const presentToast = (position: 'top' | 'middle' | 'bottom', message: string | IonicSafeString | undefined) => {
     present({
@@ -24,16 +26,25 @@ export default function Clientes() {
     setClients(prev => prev?.filter((cliente) => cliente.id !== clientId))
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const responseData = await fetch(`${process.env.REACT_APP_API_URL}/cliente/findAll`)
-      if (!responseData.ok) {
-        const response = await responseData.text();
-        return presentToast('top', response)
-      }
-      const response = await responseData.json() as Cliente[];
-      setClients(response)
+  const fetchData = async () => {
+    const responseData = await fetch(`${process.env.REACT_APP_API_URL}/cliente/findAll`)
+    if (!responseData.ok) {
+      const response = await responseData.text();
+      return presentToast('top', response)
     }
+    const response = await responseData.json() as Cliente[];
+    setClients(response)
+  }
+
+  const editClient = (index: number) => {
+    return (editedClient?: Cliente) => {
+      setClientEdited(undefined)
+      if (!editedClient) return
+      setClients(prev => prev?.map(client => client.id === index ? editedClient : client))
+    }
+  }
+
+  useEffect(() => {
     fetchData()
   }, [])
 
@@ -42,37 +53,39 @@ export default function Clientes() {
       {
         clients == null ? <IonSpinner></IonSpinner>
           :
-          <div>
-            {clients.map(({ nombre, identificacionNumero, id }, index) =>
-              <div key={index} style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                backgroundColor: '#f7f7f7',
-                color: 'black',
-                borderColor: '#dedede',
-                border: '1px solid',
-                borderRadius: '5px',
-                padding: '0 1rem'
-              }}>
-                <div>
-                  <p>
-                    Cliente: {identificacionNumero}
-                  </p>
-                  <p>
-                    Nombre: {nombre}
-                  </p>
-                </div>
-                <div>
-                  <IonButton color='primary'>
-                    <IonIcon icon={createOutline}></IonIcon>
-                  </IonButton>
-                  <IonButton onClick={() => deleteClient(id)} color='danger'>
-                    <IonIcon icon={trashOutline}></IonIcon>
-                  </IonButton>
-                </div>
-              </div>)}
-          </div>
+          clientEdited ? <RegistrarCliente client={clientEdited} editClient={editClient(clientEdited.id)} /> :
+            <div>
+              {clients.map((client, index) =>
+                <div key={index} style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  backgroundColor: '#f7f7f7',
+                  color: 'black',
+                  borderColor: '#dedede',
+                  border: '1px solid',
+                  borderRadius: '5px',
+                  padding: '0 1rem',
+                  marginBottom: '1rem'
+                }}>
+                  <div>
+                    <p>
+                      Cliente: {client.identificacionNumero}
+                    </p>
+                    <p>
+                      Nombre: {client.nombre}
+                    </p>
+                  </div>
+                  <div>
+                    <IonButton onClick={() => setClientEdited(client)} color='primary'>
+                      <IonIcon icon={createOutline}></IonIcon>
+                    </IonButton>
+                    <IonButton onClick={() => deleteClient(client.id)} color='danger'>
+                      <IonIcon icon={trashOutline}></IonIcon>
+                    </IonButton>
+                  </div>
+                </div>)}
+            </div>
       }
     </div>
   )
--- src/pages/RegistrarCliente.tsx
+++ src/pages/RegistrarCliente.tsx
@@ -1,7 +1,8 @@
 import { IonButton, IonCol, IonContent, IonHeader, IonicSafeString, IonInput, IonItem, IonLabel, IonPage, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar, useIonToast } from '@ionic/react';
+import { Dispatch, SetStateAction } from 'react';
 import { useForm } from 'react-hook-form';
 
-export default function RegistrarCliente() {
+export default function RegistrarCliente({ client, editClient }: { client?: Cliente, editClient?: (editedClient?: Cliente) => void }) {
   const {
     register,
     handleSubmit,
@@ -23,54 +24,72 @@ export default function RegistrarCliente() {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Registro de Cliente</IonTitle>
+            <IonTitle size="large">Cliente</IonTitle>
           </IonToolbar>
         </IonHeader>
         <form onSubmit={handleSubmit(async (data) => {
-          const responseData = await fetch(`${process.env.REACT_APP_API_URL}/cliente/create`, {
-            body: JSON.stringify(data),
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-          })
-          if (!responseData.ok) {
-            const response = await responseData.text();
-            return presentToast('top', response)
+          if (!client) {
+            const responseData = await fetch(`${process.env.REACT_APP_API_URL}/cliente/create`, {
+              body: JSON.stringify(data),
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+            })
+            if (!responseData.ok) {
+              const response = await responseData.text();
+              return presentToast('top', response)
+            }
+            const response = await responseData.json() as Cliente;
+            presentToast('top', `Cliente ${response.nombre} creado con éxito`)
+          } else {
+            const responseData = await fetch(`${process.env.REACT_APP_API_URL}/cliente/update`, {
+              body: JSON.stringify({ ...data, id: client.id }),
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+            })
+            if (!responseData.ok) {
+              const response = await responseData.text();
+              return presentToast('top', response)
+            }
+            const response = await responseData.json() as Cliente;
+            presentToast('top', `Cliente ${response.nombre} actualizado con éxito`)
+            editClient && editClient(response)
           }
-          const response = await responseData.json() as Cliente;
-          presentToast('top', `Cliente ${response.nombre} creado con éxito`)
         })}>
           <IonItem lines="full">
             <IonLabel position="floating">Tipo de identificación</IonLabel>
-            <IonSelect {...register('tipoIdentificacion')} placeholder="Tipo de identificación">
+            <IonSelect value={client?.tipoIdentificacion} {...register('tipoIdentificacion')} placeholder="Tipo de identificación">
               <IonSelectOption value="CEDULA">Cédula</IonSelectOption>
               <IonSelectOption value="RUC">RUC</IonSelectOption>
             </IonSelect>
           </IonItem>
           <IonItem lines="full">
             <IonLabel position="floating">Número de identificación</IonLabel>
-            <IonInput {...register('identificacionNumero')} type="text" required></IonInput>
+            <IonInput value={client?.identificacionNumero} {...register('identificacionNumero')} type="text" required></IonInput>
           </IonItem>
           <IonItem lines="full">
             <IonLabel position="floating">Nombre</IonLabel>
-            <IonInput {...register('nombre')} type="text" required></IonInput>
+            <IonInput value={client?.nombre} {...register('nombre')} type="text" required></IonInput>
           </IonItem>
           <IonItem lines="full">
             <IonLabel position="floating">Dirección</IonLabel>
-            <IonInput {...register('direccion')} type="text" required></IonInput>
+            <IonInput value={client?.direccion} {...register('direccion')} type="text" required></IonInput>
           </IonItem>
           <IonItem lines="full">
             <IonLabel position="floating">Teléfono</IonLabel>
-            <IonInput {...register('telefono')} type="text" required></IonInput>
+            <IonInput value={client?.telefono} {...register('telefono')} type="text" required></IonInput>
           </IonItem>
           <IonItem lines="full">
             <IonLabel position="floating">Correo</IonLabel>
-            <IonInput {...register('correoElectronico')} type="email" required></IonInput>
+            <IonInput value={client?.correoElectronico} {...register('correoElectronico')} type="email" required></IonInput>
           </IonItem>
           <IonRow>
             <IonCol>
-              <IonButton type="submit" color="primary" expand="block">Crear</IonButton>
+              <IonButton type="submit" color="primary" expand="block">{client ? 'Editar' : 'Crear'}</IonButton>
+              {client && <IonButton onClick={() => editClient && editClient(undefined)} color="danger" expand="block">Cancelar</IonButton>}
             </IonCol>
           </IonRow>
         </form>
